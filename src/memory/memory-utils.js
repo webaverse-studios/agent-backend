7180e1e4b477e4da892005ae13f83dd906dd8e74
olaf <jkraasch@uos.de>
2023-03-03 17:32:15 -0800

    Implemented fake memory transformation functions saving the results does not work yet, will look into it on Monday

--- src/memory/memory-utils.js
+++ src/memory/memory-utils.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 import { app } from "../app.js";
 import { agents } from "../gun_src/gun_app.js";
 import { flipCoin } from "../utils.js";
@@ -20,6 +21,64 @@ export async function getMemoryFromGunDB(agent, timestamp) {
   return agents.get(agent).get("memories").get(timestamp);
 }
 
+
+function rgbToHsl(r, g, b) {
+  (r /= 255), (g /= 255), (b /= 255);
+  const max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  let h,
+    s,
+    l = (max + min) / 2;
+
+  if (max === min) {
+    h = s = 0; // achromatic
+  } else {
+    const d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+    h /= 6;
+  }
+
+  return [h, s, l];
+}
+
+function hslToRgb(hslElement, hslElement2, hslElement3) {
+  let h = hslElement;
+  let s = hslElement2;
+  let l = hslElement3;
+  let r, g, b;
+
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    const hue2rgb = (p, q, t) => {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
+
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+
+  return [r * 255, g * 255, b * 255];
+}
 
 export async function remember(agent, timestamp) {
   // get memory from the gun database and return them as a javascript object
@@ -49,16 +108,49 @@ export async function remember(agent, timestamp) {
 
   async function rememberImage(modality) {
     // get the image from the feathers database
-    const image = await app.service("image-cache").get(modality);
-
+    const image_response = await app.service("image-cache").get(modality);
+    const img_uri = image_response.uri;
+    // img uri into an int array buffer called image
+    const arrayBuffer = await fetch(img_uri).then(response => response.arrayBuffer());
+    const image = new Uint8ClampedArray(arrayBuffer);
     if (flipCoin(0.5)) {
       // if the coin flip is true we return the image
       return image;
     } else {
-      // else we perform a random transformation on the image
+      // else we perform a random transformation on the image e.g. make image grayscale or shift hue
       const transformations = [
-        (image) => image
-      ];
+          // grayscale image
+          (image) => {
+            // iterate over all pixels
+            for (let i = 0; i < image.length; i += 4) {
+              // calculate average
+              const avg = (image[i] + image[i + 1] + image[i + 2]) / 3;
+              // set each pixel to average
+              image[i] = avg;
+              image[i + 1] = avg;
+              image[i + 2] = avg;
+            }
+            return image;
+          },
+          // hue shift
+          (image) => {
+            // iterate over all pixels
+            for (let i = 0; i < image.length; i += 4) {
+              // perform hue shift
+              const r = image[i];
+              const g = image[i + 1];
+              const b = image[i + 2];
+              const hsl = rgbToHsl(r, g, b);
+              hsl[0] = (hsl[0] + 0.5) % 1;
+              const rgb = hslToRgb(hsl[0], hsl[1], hsl[2]);
+              image[i] = rgb[0];
+              image[i + 1] = rgb[1];
+              image[i + 2] = rgb[2];
+            }
+            return image;
+          },
+        ]
+      ;
       const randomTransformation = transformations[Math.floor(Math.random() * transformations.length)];
       return randomTransformation(image);
     }
@@ -66,7 +158,12 @@ export async function remember(agent, timestamp) {
 
   async function rememberVoice(modality) {
     // get the audio from the feathers database
-    const voice = app.service("voice-cache").get(modality);
+    const voice_response = await app.service("voice-cache").get(modality);
+    const voice_uri = voice_response.uri;
+
+    // voice uri into an array buffer called voice
+    const arrayBuffer = await fetch(voice_uri).then(response => response.arrayBuffer());
+    const voice = new Uint8ClampedArray(arrayBuffer);
 
     if (flipCoin(0.5)) {
       // if the coin flip is true we return the audio
@@ -74,7 +171,19 @@ export async function remember(agent, timestamp) {
     } else {
       // else we perform a random transformation on the audio
       const transformations = [
-        (voice) => voice
+        // reverse audio
+        (voice) => {
+          return voice.reverse();
+        },
+        // pitch shift
+        (voice) => {
+          // shift pitch by 1 octave
+          for (let i = 0; i < voice.length; i += 2) {
+            voice[i] = voice[i] * 2;
+          }
+          return voice;
+        }
+
       ];
       const randomTransformation = transformations[Math.floor(Math.random() * transformations.length)];
       return randomTransformation(voice);
@@ -92,7 +201,9 @@ export async function remember(agent, timestamp) {
       memory.text = await rememberText(modality);
     }
     if (key === "image") {
-      memory.image = await rememberImage(modality);
+      const image =  await rememberImage(modality);
+      console.log("IMAGE AFTER TRANSFORMATION:", image)
+      memory.image = image;
     }
     if (key === "voice") {
       memory.voice = await rememberVoice(modality);
@@ -112,12 +223,10 @@ export async function addMemory(agent, timestamp, data) {
   for (const key of Object.keys(data)) {
     const modality = data[key];
     if (key === "text") {
-      console.log("text:", modality);
       const { _id } = await app.service("text-cache").create(modality);
       await addMemoryToGunDB(agent, timestamp, { text: _id });
     }
     if (key === "image") {
-      console.log("image:", modality);
       const { _id } = await app.service("image-cache").create(modality);
       await addMemoryToGunDB(agent, timestamp, { image: _id });
     }
--- test/memory.test.js
+++ test/memory.test.js
@@ -1,3 +1,4 @@
+import assert from "assert";
 import { app } from "../src/app.js";
 import { gun } from "../src/gun_src/gun_app.js";
 import fs from "fs";
@@ -24,17 +25,35 @@ const main = async () => {
   const data = {agentIDs, prompt, text, timestamp:timestamp}
 
   // cache a test image in the feathers database after loading test.jpg and turning it into a blob
-  const imageBlob = createBlobFromImageFile("./test.jpg")
+  const imageBlob = createBlobFromImageFile("test/test.jpg")
   const image_data = {agentID: "bob", imageBlob: imageBlob, timestamp:timestamp}
 
   // open wav file and turn into blob
-  const wavFile = fs.readFileSync("./test.wav")
+  const wavFile = fs.readFileSync("test/test.wav")
   const wavBlob = new Blob([wavFile], {type: 'audio/wav'})
   const voice_data = {agentIDs: "bob", prompt:"test-prompt", audioBlob: wavBlob, timestamp:timestamp}
   // add a test memory to the gun database
   await addMemory("bob", timestamp, {text: data, image: image_data, voice: voice_data})
   const memory = await remember("bob", timestamp)
   console.log("MEMORY:", memory)
+  // store the Uint8ClampedArray of the image found under memory.image in a file called memory_image.jpg
+  // write the buffer to a file
+  const imageBuffer = Buffer.from(memory.image)
+  fs.writeFileSync('memory_image.jpg', imageBuffer, (err) => {
+    if (err) throw err;
+    console.log('Memory image saved to file');
+  });
+
+
+  // store the Uint8ClampedArray of the voice found under memory.voice in a file called memory_voice.wav
+  const voiceBuffer = Buffer.from(memory.voice)
+  // write the buffer to a file
+  fs.writeFileSync('memory_audio.wav', voiceBuffer, (err) => {
+    if (err) throw err;
+    console.log('Memory audio saved to file');
+  });
+
+
 
 }
 await main()
